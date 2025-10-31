0cd23134db6edeca670c350d8ac7f3467007b14a
asenetlazcano.thz@gmail.com <asenetlazcano.thz@gmail.com>
2025-10-31 15:10:06 -0600

    Proyecyo inicial

---
+++ src/main/java/com/example/demo/ScapeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScapeApplication.class, args);
+	}
+
+}
